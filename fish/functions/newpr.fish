4a19411b17a6142f2c98da55710493d9999afb63
peterzernia <peter@peterzernia.com>
2021-06-21 12:51:42 +0200

    update newpr function

--- fish/functions/newpr.fish
+++ fish/functions/newpr.fish
@@ -1,3 +1,3 @@
 function newpr
-	echo (git config --get remote.origin.url | sed "s/:/\//" | sed "s/git@/https:\/\//" | sed "s/\.git/*/" | sed "s/*/\/compare/")
+	google-chrome-stable (git config --get remote.origin.url | sed "s/:/\//" | sed "s/git@/https:\/\//" | sed "s/\.git/*/" | sed "s/*/\/compare\/master.../")""(git rev-parse --abbrev-ref HEAD)
 end
