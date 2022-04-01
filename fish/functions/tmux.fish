95d0fcd8a48c9ea49bcf83752f776ce453a90e96
peterzernia <peter.zernia@fincompare.com>
2022-04-01 14:29:30 +0200

    symlink vimrc

--- fish/functions/tmux.fish
+++
@@ -1,3 +0,0 @@
-function tmux --description "Starts tmux with config located in ~/.config/tmux/.tmux.conf"
-command tmux -f ~/.config/tmux/.tmux.conf $argv
-end
---
+++ vim/.vimrc
@@ -0,0 +1,1 @@
+/Users/peterzernia/.vimrc
